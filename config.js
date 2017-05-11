1d462f5b22a5404b8ddeb1d240c93c91c604fc35
lidad <497718909@qq.com>
2017-05-11 16:52:37 +0800

    climb five page info

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 module.exports = {
-  url: 'http://sh.lianjia.com/chengjiao/'
+  url: 'http://sh.lianjia.com/chengjiao/d'
 }
--- index.js
+++ index.js
@@ -14,19 +14,35 @@ function getHtml(url) {
   })
 }
 
-getHtml(config.url).then(($) => {
-  let result = [];
-  $('.info-table').each((i, block) => {
-    const row = $(block).find('.info-row');
-    const item = {
-      homeModel: $(row[0]).find('.text').text().replace(/\s/g, ''),
-      dealDate: $(row[1]).find('.deal-item').text(),
-      dealAmount: $(row[1]).find('.price-item  .strong-num').text(),
-      perMeter: $(row[2]).find('.price-item').text(),
-      zone: $(row[2]).find('.row2-text').text().replace(/\s/g, '')
-    }
-    result.push(item)
-  });
+function getOnePage(url) {
+  return getHtml(url).then(($) => {
+    let result = [];
+    $('.info-table').each((i, block) => {
+      const row = $(block).find('.info-row');
+      const item = {
+        homeModel: $(row[0]).find('.text').text().replace(/\s/g, ''),
+        dealDate: $(row[1]).find('.deal-item').text(),
+        dealAmount: $(row[1]).find('.price-item  .strong-num').text(),
+        perMeter: $(row[2]).find('.price-item').text(),
+        zone: $(row[2]).find('.row2-text').text().replace(/\s/g, '')
+      }
+      result.push(item)
+    });
+    return result;
+  })
+}
 
-  console.log(result)
+let index = 1;
+let url = '';
+let prs = [];
+for (; index < 5; index++) {
+  url = config.url + index;
+  prs.push(getOnePage(url))
+}
+console.log('loading....')
+
+Promise.all(prs).then((res) => {
+  res.forEach((result) => {
+    console.log(result);
+  })
 })
